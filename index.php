fcefb99b251d246ef9bfb79f5ffaf4e510175e0d
Alberto Ortiz Flores <aortiz@nyu.edu>
2020-12-22 15:45:49 -0500

    DLTSHIDVL-87

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/include/common.php';
+require_once __DIR__ . '/include/ISO639.php';
 use Bramus\Router\Router;
 
 $router = new Router();
